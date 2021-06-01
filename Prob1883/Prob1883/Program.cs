ed81145e130e5d7900208629806b706e5844cdf2
Scott Oberman <scottoberman@gmail.com>
2021-05-31 21:15:37 -0700

    1883 In almost functional state (skips after the first bfs fail it seems).

--- Prob1883/Prob1883/Program.cs
+++ Prob1883/Prob1883/Program.cs
@@ -1,91 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Prob1883
 {
-    //public class Solution
-    //{
-    //    public int MinSkips(int[] dist, int speed, int hoursBefore)
-    //    {
+    public class Solution
+    {
+        int[] dist;
+        int speed;
+        int hoursBefore;
 
+        public int MinSkips(int[] dist, int speed, int hoursBefore)
+        {
+            int fewestSkips;
+            this.dist = dist;
+            this.hoursBefore = hoursBefore;
+            this.speed = speed;
+            bool[] initSkip = Enumerable.Repeat(false, dist.Length).ToArray();
+            bool[] endSkip  = Enumerable.Repeat(true, dist.Length).ToArray();
 
-    //        int minSkipsSoFar = 0;
-    //        bool minSkipFound = false;
-    //        Dictionary<int, int> skips = new Dictionary<int, int>();
+            // Just initially check the base node
+            // and see if we don't need to progress.
+            if (CheckNode(initSkip))
+                return 0;
+            // Check if its even possible to reach
+            // our destination within the time
+            // constraints.
+            else if (!CheckNode(endSkip))
+                return -1;
 
-    //        // Step 1: Skip all stops to see if its even possible
-    //        // to arrive on time.
-    //        double timeToTravelNoSkip = dist.Sum() / speed;
-    //        if (timeToTravelNoSkip > hoursBefore)
-    //            return -1;
+            bool[] result = CheckRow(0, 0, 0, initSkip);
 
-    //        // Step 2Maybe: Check time without skipping anything
-    //        if ()
+            fewestSkips = result != null ? result.Count(b => b == true) : -1;
 
+            return fewestSkips;
+        }
 
+        private bool[] CheckRow(int depthCur, int curNodeNum, int distInd, bool[] curSkipsCheck)
+        {
+            int numChildren = dist.Length - depthCur - distInd;
+            bool[][] children = new bool[numChildren][];
+            // Generate children and check that child
+            // to see if it fullfills our time requiremnts.
+            for (int x = 0; x < numChildren; x++)
+            {
+                children[x] = new bool[dist.Length];
+                curSkipsCheck.CopyTo(children[x], 0);
 
+                int curSkipTracker = (curNodeNum) % dist.Length;
+                while (children[x][curSkipTracker])
+                {
+                    curSkipTracker = (curSkipTracker + 1) % dist.Length;
+                }
 
+                children[x][curSkipTracker] = true;
 
-    //        // Step 2maybe: Attempt to skip the largest value in the array?
-    //        //         (This may be complicated by the fact that
-    //        //          a user must wait until the next full hour to leave if not skipping a thing)
-    //        while (!minSkipFound && minSkipsSoFar < dist.Length)
-    //        {
-    //            skips.Max()
+                if(CheckNode(children[x]))
+                    return children[x];
+            }
 
-    //        }
+            // Explore children
+            for (int x = 0; x < numChildren - 1; x++)
+            {
+                bool[] childSuccess = CheckRow(depthCur + 1, curNodeNum + x, distInd + 1, children[x]);
+                if (childSuccess != null)
+                    return children[x];
+            }
 
-    //        // Step 3: Repeat step 2 until done?
-    //    }
+            return null;
+        }
 
-    //    /// <summary>
-    //    /// Initially, just brute force permutations 
-    //    /// until we find fewest skips. (Eventually, wanna do
-    //    /// sort of a BFS rather than this sort of random
-    //    /// search).
-    //    /// </summary>
-    //    /// <param name="dist"></param>
-    //    /// <param name="speed"></param>
-    //    /// <param name="hoursBefore"></param>
-    //    /// <returns></returns>
-    //    private int FindFewestSkips(int[] dist, int speed, int hoursBefore)
-    //    {
-    //        bool[] locsSkipped = new bool[dist.Length];
-    //        int totalPerms = (int)Math.Pow(2, dist.Length);
+        private bool CheckNode(bool[] skipToCheck)
+        {
+            double timeTotal = 0;
+            for (int x = 0; x < dist.Length; x++)
+            {
+                timeTotal = timeTotal + (double)dist[x] / speed;
+                if (!skipToCheck[x] && x != dist.Length - 1)
+                    timeTotal = Math.Ceiling(timeTotal);
+            }
 
-    //        for(int x = 0; x < totalPerms; x++)
-    //        {
-    //            for(int y = 0; y < dist.Length; y++)
-    //            {
-    //                if ((y - x) % dist.Length == 0)
-    //                    locsSkipped[y] = true;
-    //                else
-    //                    locsSkipped[y] = false;
-    //            }
-    //        }
-    //    }
-    //}
-
+            return timeTotal <= this.hoursBefore;
+        }
+    }
 
     class Program
     {
-        static void TestLoopStuff()
-        {
-            int[] dist = new int[3];
-            dist[0] = 0;
-            dist[1] = 1;
-            dist[2] = 2;
-
-            TreeNode perms = new TreeNode(dist.Length);
-
-
-        }
         static void Main(string[] args)
         {
-            TestLoopStuff();
+            int speed = 2;
+            int hoursBefore = 10;
+            int[] dist = new int[4];
+            dist[0] = 7;
+            dist[1] = 3;
+            dist[2] = 5;
+            dist[3] = 5;
+            Solution sol = new Solution();
+            var result = sol.MinSkips(dist, speed, hoursBefore);
             Console.Read();
         }
     }
