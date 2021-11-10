a0e8c33978d81c4d100782b75b6403a6d3790638
Scott Oberman <scottoberman@gmail.com>
2021-11-10 00:53:21 -0800

    prob122 done

--- Prob122/solution.py
+++ Prob122/solution.py
@@ -3,36 +3,37 @@ class Solution:
     def maxProfit(self, prices) -> int:
         maxProfit = 0
 
-        daysAndPrices = enumerate(prices)
+        curMin = prices[0]
+        curMax = prices[0]
 
-        daysAndPricesSorted =  sorted(daysAndPrices, key=lambda x: x[1], reverse=True) # change x[1] to x[0]
-
-        x = 0
-        maxFound = False
-
-        # Using whiles is more hideous than breaking fors
-        # but just doing so for ez single return.
-        while not maxFound and x < len(daysAndPricesSorted):
-            y = len(daysAndPricesSorted) - 1
-            while not maxFound and y > x:
-                potentialProfit = daysAndPricesSorted[x][1] -  daysAndPricesSorted[y][1]
-                dayBuy = daysAndPricesSorted[x][0]
-                daySell = daysAndPricesSorted[y][0]
-                if potentialProfit > maxProfit and \
-                    dayBuy < daySell:
-                    maxProfit = potentialProfit
-                    maxFound = True
+        lookingForMin = True
+        for x in range(len(prices)):
+            price = prices[x]
+            if lookingForMin:
+                if curMin > price:
+                    curMin = price
                 else:
-                    y -= 1
+                    curMax = price
+                    lookingForMin = not lookingForMin
+            if not lookingForMin:
+                if curMax < price:
+                    curMax = price
+                else:
+                    # "Selling" a stock
+                    lookingForMin = not lookingForMin
+                    maxProfit += curMax - curMin
+                    curMax = price
+                    curMin = price
 
-            if not maxFound:
-                x += 1
-        
         return maxProfit
     
-
+# 5, 3, 2, 1, 0, -1, -2, -3
 def Testing():
     sol = Solution()
     print(sol.maxProfit([3, 5, 7, 9, 11, 3, 4, 5]))
+    print(sol.maxProfit([7,1,5,3,6,4]))
+    print(sol.maxProfit([1,2,3,4,5]))
+    print(sol.maxProfit([7,6,4,3,1]))
+    print(sol.maxProfit([1]))
 
 Testing()
